2b8282d7b04fe7045eb7a24f939dea4028f4f81e
themdshakibul <mdshakibul88240@gmail.com>
2026-02-21 10:55:04 +0600

    current status

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let thrivingList = [];
 let strugglingList = [];
+let CurrentStatu = "all";
 
 // count section
 let total = document.getElementById("total");
@@ -36,10 +37,23 @@ function toggleStyle(id) {
   sturgglingFilterBtn.classList.add("btn", "text-2xl");
 
   const selected = document.getElementById(id);
-  console.log(selected);
+  CurrentStatu = id;
 
   selected.classList.remove("btn");
   selected.classList.add("bg-black", "text-white");
+
+  if (id == "thriving-filter-btn") {
+    allCardsSection.classList.add("hidden");
+    filteradSection.classList.remove("hidden");
+    renderThriving();
+  } else if (id == "all-filter-btn") {
+    allCardsSection.classList.remove("hidden");
+    filteradSection.classList.add("hidden");
+  } else if (id == "sturggling-filter-btn") {
+    allCardsSection.classList.add("hidden");
+    filteradSection.classList.remove("hidden");
+    renderStruggling();
+  }
 }
 
 mainContainer.addEventListener("click", function (event) {
@@ -52,11 +66,13 @@ mainContainer.addEventListener("click", function (event) {
     const statu = parentNode.querySelector(".statu").innerText;
     const notes = parentNode.querySelector(".notes").innerText;
 
+    parentNode.querySelector(".statu").innerText = "thrive";
+
     const cardInfo = {
       plantName,
       light,
       water,
-      statu,
+      statu: "thrive",
       notes,
     };
     console.log(cardInfo);
@@ -69,11 +85,56 @@ mainContainer.addEventListener("click", function (event) {
       thrivingList.push(cardInfo);
     }
 
-    renderThriving();
+    strugglingList = strugglingList.filter(
+      (item) => item.plantName != cardInfo.plantName,
+    );
+
+    if (CurrentStatu == "struggling-filter-btn") {
+      renderStruggling();
+    }
+
+    calculateCount();
+  } else if (event.target.classList.contains("struggling-btn")) {
+    const parentNode = event.target.parentNode.parentNode;
+
+    const plantName = parentNode.querySelector(".plantName").innerText;
+    const light = parentNode.querySelector(".light").innerText;
+    const water = parentNode.querySelector(".water").innerText;
+    const statu = parentNode.querySelector(".statu").innerText;
+    const notes = parentNode.querySelector(".notes").innerText;
+
+    parentNode.querySelector(".statu").innerText = "struggling";
+
+    const cardInfo = {
+      plantName,
+      light,
+      water,
+      statu: "struggling",
+      notes,
+    };
+    console.log(cardInfo);
+
+    const plantExist = strugglingList.find(
+      (item) => item.plantName == cardInfo.plantName,
+    );
+
+    if (!plantExist) {
+      strugglingList.push(cardInfo);
+    }
+
+    thrivingList = thrivingList.filter(
+      (item) => item.plantName != cardInfo.plantName,
+    );
+
+    if (CurrentStatu == "thriving-filter-btn") {
+      renderThriving();
+    }
+
+    calculateCount();
   }
 });
 
-//
+// thrivingList
 function renderThriving() {
   filteradSection.innerHTML = "";
 
@@ -81,11 +142,10 @@ function renderThriving() {
     let div = document.createElement("div");
     div.className = `flex justify-between p-8 border rounded-md mb-8`;
     div.innerHTML = `
-        // <!-- main par 1 -->
           <div class="card">
             <div class="space-y-5">
               <div>
-                <h2 class="plantName text-4xl font-semibold">Plant name 1</h2>
+                <h2 class="plantName text-4xl font-semibold">${thrive.plantName}</h2>
                 <h2 class="latinName text-xl font-medium">Latin name</h2>
               </div>
               <div class="flex gap-5">
@@ -93,7 +153,7 @@ function renderThriving() {
                 <p class="water btn btn-soft">Weekly</p>
               </div>
               <h2 class="statu btn btn-soft btn-outline text-xl">
-                NO Applicable
+                ${thrive.statu}
               </h2>
               <p class="notes">New leaf unfurling by the east window</p>
               <div class="flex gap-5">
@@ -106,12 +166,56 @@ function renderThriving() {
               </div>
             </div>
           </div>
-          // <!-- main par 2 -->
+
           <div>
             <button class="delet-btn btn btn-soft btn-secondary btn-lg">
               Deleat
             </button>
           </div>
       `;
+    filteradSection.appendChild(div);
+  }
+}
+
+// strugglingList
+function renderStruggling() {
+  filteradSection.innerHTML = "";
+
+  for (let sturggling of strugglingList) {
+    let div = document.createElement("div");
+    div.className = `flex justify-between p-8 border rounded-md mb-8`;
+    div.innerHTML = `
+          <div class="card">
+            <div class="space-y-5">
+              <div>
+                <h2 class="plantName text-4xl font-semibold">${sturggling.plantName}</h2>
+                <h2 class="latinName text-xl font-medium">Latin name</h2>
+              </div>
+              <div class="flex gap-5">
+                <p class="light btn btn-soft">Bright Indicat</p>
+                <p class="water btn btn-soft">Weekly</p>
+              </div>
+              <h2 class="statu btn btn-soft btn-outline text-xl">
+                ${sturggling.statu}
+              </h2>
+              <p class="notes">New leaf unfurling by the east window</p>
+              <div class="flex gap-5">
+                <button class="thriving-btn btn btn-outline btn-secondary">
+                  Thriving
+                </button>
+                <button class="struggling-btn btn btn-outline btn-success">
+                  Struggling
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <button class="delet-btn btn btn-soft btn-secondary btn-lg">
+              Deleat
+            </button>
+          </div>
+      `;
+    filteradSection.appendChild(div);
   }
 }
